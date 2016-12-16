374d0f2a1e801ac6544c9b68144112c1cf4a85ad
DenisGz <gazizyanov@mail.ru>
2016-12-16 20:16:06 +0300

    заправка

--- zapravka.js
+++ zapravka.js
@@ -1,15 +1,15 @@
 var bak = 53;
 var bakOst=[];
-bakOst[0]= prompt('Сколько бензина в баке, %?', 50);
+bakOst[0]= prompt('Сколько бензина в баке, %?', 62);
 var probeg = [];
-probeg[0] = prompt('Пробег сейчас, км?', 17610);
+probeg[0] = prompt('Пробег сейчас, км?', 17651);
 var bakOstLitr = bak*bakOst/100;
 var bakRenew = Math.round((bak - bakOstLitr)*100)/100;
 alert ('Сейчас у вас в баке ' + bakOstLitr + ' литров, до полного бака вам нужно заправить ' + bakRenew);
 
 
-probeg[1] = prompt('Пробег сейчас, км?', 17610);
-bakOst[1]= prompt('сколько бензина в баке, %?', 50);
+probeg[1] = prompt('Пробег сейчас, км?', 17998);
+bakOst[1]= prompt('сколько бензина в баке, %?', 1);
 var rashod = [];
 rashod[0] =Math.round(((((bakOst[0] - bakOst[1])*0.53) / (probeg[1] - probeg[0]))*100)*100)/100;
 
