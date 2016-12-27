aaec84241c143c0fe71ea863e564ff7fe7cdd543
DenisGz <gazizyanov@mail.ru>
2016-12-27 16:12:13 +0300

    applyAll через reduceCall

---
+++ reduceCall.js
@@ -0,0 +1,23 @@
+'use strict'
+function sum() {
+	var red = [].reduce;
+	var csd = red.call(arguments, function(a,b) {
+		return a + b;
+	});
+return csd;
+	}
+	
+function mul() {
+  return [].reduce.call(arguments, function(a, b) {
+    return a * b;
+  });
+}
+
+function applyAll(func) {
+    return func.apply(this, [].slice.call(arguments, 1));
+  }
+
+alert( applyAll(sum, 1, 2, 3) ); // 6
+alert( applyAll(mul, 2, 3, 4) ); // 24
+alert( applyAll(Math.max, 2, -2, 3) ); // 3
+alert( applyAll(Math.min, 2, -2, 3) ); // -2
